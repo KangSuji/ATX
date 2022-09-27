a15d0245eb887fce1b72add7f421b55b886ed5ec
SUJI <sukang0608@naver.com>
2022-09-27 14:10:42 +0900

    mobile header add

--- js/index.js
+++ js/index.js
@@ -6,6 +6,27 @@ $(document).ready(function () {
     $(this).children(".dep2").stop().slideUp();
   });
 
+  //토글메뉴로변경
+  $(".dep1 .dep1_a").click(function () {
+    $(".dep2").hide();
+    $(this).next(".dep2").slideToggle();
+  });
+  $(".open_btn").click(function () {
+    if ($(".mobile-gnb").hasClass("hide")) {
+      $(".mobile-gnb_wrap").fadeIn();
+      $(".mobile-gnb").removeClass("hide");
+      $(".mobile-bg").removeClass("hide");
+      $(".mo-logo").attr("src", "../images/logo.png");
+      $(".mo-menu").attr("src", "../images/btn_close.png");
+    } else {
+      $(".mobile-gnb_wrap").fadeOut();
+      $(".mobile-gnb").addClass("hide");
+      $(".mobile-bg").addClass("hide");
+      $(".mo-logo").attr("src", "../images/logo.png");
+      $(".mo-menu").attr("src", "../images/btn-menu-m.png");
+    }
+  });
+
   $(".main-visual-scroll img").click(function () {
     $("html, body").animate({ scrollTop: $(".main-business").offset().top }, 500);
   });
